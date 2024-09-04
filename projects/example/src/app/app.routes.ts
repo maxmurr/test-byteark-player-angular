21e0d599edf8b3e9ef54885aabd148bb27297355
maxmurr <maxmurr.m@gmail.com>
2024-09-05 03:17:22 +0700

    refactor: change link and remove route

--- projects/example/src/app/app.routes.ts
+++ projects/example/src/app/app.routes.ts
@@ -1,39 +1,39 @@
 import { Routes } from '@angular/router';
 import { SampleBlogComponent } from './sample-blog/sample-blog.component';
-import { SampleFluidComponent } from './sample-fluid/sample-fluid.component';
-import { SampleFillComponent } from './sample-fill/sample-fill.component';
-import { SampleFullComponent } from './sample-full/sample-full.component';
-import { SampleLazyloadComponent } from './sample-lazyload/sample-lazyload.component';
-import { SampleAutoplayComponent } from './sample-autoplay/sample-autoplay.component';
-import { SampleAdsComponent } from './sample-ads/sample-ads.component';
+// import { SampleFluidComponent } from './sample-fluid/sample-fluid.component';
+// import { SampleFillComponent } from './sample-fill/sample-fill.component';
+// import { SampleFullComponent } from './sample-full/sample-full.component';
+// import { SampleLazyloadComponent } from './sample-lazyload/sample-lazyload.component';
+// import { SampleAutoplayComponent } from './sample-autoplay/sample-autoplay.component';
+// import { SampleAdsComponent } from './sample-ads/sample-ads.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: SampleBlogComponent,
   },
-  {
-    path: 'fluid',
-    component: SampleFluidComponent,
-  },
-  {
-    path: 'fill',
-    component: SampleFillComponent,
-  },
-  {
-    path: 'full',
-    component: SampleFullComponent,
-  },
-  {
-    path: 'lazyload',
-    component: SampleLazyloadComponent,
-  },
-  {
-    path: 'autoplay',
-    component: SampleAutoplayComponent,
-  },
-  {
-    path: 'ads',
-    component: SampleAdsComponent,
-  },
+  // {
+  //   path: 'fluid',
+  //   component: SampleFluidComponent,
+  // },
+  // {
+  //   path: 'fill',
+  //   component: SampleFillComponent,
+  // },
+  // {
+  //   path: 'full',
+  //   component: SampleFullComponent,
+  // },
+  // {
+  //   path: 'lazyload',
+  //   component: SampleLazyloadComponent,
+  // },
+  // {
+  //   path: 'autoplay',
+  //   component: SampleAutoplayComponent,
+  // },
+  // {
+  //   path: 'ads',
+  //   component: SampleAdsComponent,
+  // },
 ];
